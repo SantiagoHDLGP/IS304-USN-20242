f1c1c7aa8f0aaabfe0445147400898bc50b646c8
SANTIAGODHDLGPT <edsacardenasv@unimonserrate.edu.co>
2024-08-28 20:04:22 -0500

    Update taller.py By Santiago Cardenas

--- SESION03/taller.py
+++ SESION03/taller.py
@@ -3,3 +3,40 @@ Crear un programa que permita crear objetos de la clase CuentaBancaria, cuyos at
 Aplicar encapsulamiento y definir los métodos apropiados para controlar y gestionar los atributos de los objetos creados.
 Utilizar un menu para las diferentes opciones, tales como aperturaCta, consignar, retirar y transferencia entre otros.
 '''
+class CuentaBancaria
+    
+    def __init__(self, NumeroCta=0, NombreCliente, SaldoCta=0, FechaApertura=0, UltimoRetiro=0, UltimaConsignacion=0):
+        self.__NumeroCta = NumeroCta
+        self.__NombreCliente = Nombrecliente
+        self.__SaldoCta = SaldoCta
+        self.__FechaApertura = FechaApertura
+        self.__UltimoRetiro = UltimoRetiro
+        self.__UltimaConsignacion = UltimaConsignacion 
+        
+    def set(self, NumeroCta, NombreCliente, SaldoCta, FechaApertura, UltimoRetiro, UltimaConsignacion):
+        self.__NumeroCta = NumeroCta
+        self.__NombreCliente = Nombrecliente
+        self.__SaldoCta = SaldoCta
+        self.__FechaApertura = FechaApertura
+        self.__UltimoRetiro = UltimoRetiro
+        self.__UltimaConsignacion = UltimaConsignacion 
+        
+    def set__NumeroCta(self, x):
+        self.__NumeroCta = 
+        
+    def set__NombreCliente(self, y):
+        self.__NombreCliente = 
+        
+    def set__SaldoCta(self, x):
+        self.__SaldoCta = 
+        
+    def set__FechaApertura(self, x):
+        self.__FechaApertura = 
+        
+    def set__UltimoRetiro(self, x):
+        self.__UltimoRetiroa = 
+
+    def set__UltimaConsignacion(self, x):
+        self.__UltimaConsignacion =   
+
+
