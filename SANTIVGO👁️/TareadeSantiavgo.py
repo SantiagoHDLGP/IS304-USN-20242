5c73c18761b3af6306ed6ddbd412ef582ed5566a
SANTIAGODHDLGPT <edsacardenasv@unimonserrate.edu.co>
2024-08-28 19:56:22 -0500

    TareadeSantiavgo.py
    Mi tarea

---
+++ SANTIVGO👁️/TareadeSantiavgo.py
@@ -0,0 +1,43 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+class CuentaBancaria
+    
+    def __init__(self, NumeroCta=0, NombreCliente, SaldoCta=0, FechaApertura=0, UltimoRetiro=0, UltimaConsignacion=0):
+        self.__NumeroCta = NumeroCta
+        self.__NombreCliente = Nombrecliente
+        self.__SaldoCta = SaldoCta
+        self.__FechaApertura = FechaApertura
+        self.__UltimoRetiro = UltimoRetiro
+        self.__UltimaConsignacion = UltimaConsignacion 
+        
+    def set(self, NumeroCta, NombreCliente, SaldoCta, FechaApertura, UltimoRetiro, UltimaConsignacion):
+        self.__NumeroCta = NumeroCta
+        self.__NombreCliente = Nombrecliente
+        self.__SaldoCta = SaldoCta
+        self.__FechaApertura = FechaApertura
+        self.__UltimoRetiro = UltimoRetiro
+        self.__UltimaConsignacion = UltimaConsignacion 
+        
+    def set__NumeroCta(self, x):
+        self.__NumeroCta = 
+        
+    def set__NombreCliente(self, y):
+        self.__NombreCliente = 
+        
+    def set__SaldoCta(self, x):
+        self.__SaldoCta = 
+        
+    def set__FechaApertura(self, x):
+        self.__FechaApertura = 
+        
+    def set__UltimoRetiro(self, x):
+        self.__UltimoRetiroa = 
+
+    def set__UltimaConsignacion(self, x):
+        self.__UltimaConsignacion =   
