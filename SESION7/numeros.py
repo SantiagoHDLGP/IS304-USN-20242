5e4ec56000e6e0f5db172724779a1850b24679be
SANTIAGODHDLGPT <edsacardenasv@unimonserrate.edu.co>
2024-09-25 19:43:18 -0500

    Update numeros.py

--- SESION7/numeros.py
+++ SESION7/numeros.py
@@ -1,3 +1,4 @@
+# Escríbeme un programa que cree una lista con los números del 1 al 100 (ambos incluidos), sustituyendo posteriormente los elementos siguientes: Posiciones múltiplos de 3 por la palabra "fizz" , Posiciones múltiplos de 5 por la palabra "buzz" , Posiciones múltiplos de 3 y de 5 a la vez  por la palabra "fizzbuzz" 
 # Crear una lista del 1 al 100
 lista = list(range(1, 101))
 
